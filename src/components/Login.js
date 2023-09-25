439da7f93766fc2d1dfefa763ddde40476aaec58
gayusuryu <gayuksg1990@gmail.com>
2023-09-25 09:44:04 +0530

    auth firebase

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,23 @@
 import React, { useRef, useState } from "react";
 import Header from "./Header";
-import {checkValidData } from "../utils/validate";
+import { checkValidData } from "../utils/validate";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
 
 const Login = () => {
   const [isSignInForm, setSignInForm] = useState(true);
 
-  const [errorMessage,setErrorMessage] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const toggleSignInForm = () => {
     setSignInForm(!isSignInForm);
@@ -14,19 +26,85 @@ const Login = () => {
   const email = useRef(null);
   const password = useRef(null);
 
-  const name = useRef('');
-  const confirmPassword = useRef('');
+  const name = useRef("");
+  const confirmPassword = useRef("");
 
   const handleButtonClick = () => {
     //Validate form
     console.log(email.current.value);
     console.log(password.current.value);
-    console.log(name.current.value);
-    console.log(confirmPassword.current.value);
-    const message = checkValidData(email.current.value, password.current.value,name?.current.value,confirmPassword?.current.value);
+    // console.log(name?.current.value);
+    // console.log(confirmPassword?.current.value);
+    const message = checkValidData(
+      email.current.value,
+      password.current.value,
+      name?.current.value,
+      confirmPassword?.current.value
+    );
     setErrorMessage(message);
 
+    if (message) return;
+    //or use like this if(!message){ }
     //Sign In/Sign Up
+    if (!isSignInForm) {
+      //Sign Up Logic
+      console.log("signinnn");
+      createUserWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          updateProfile(user, {
+            displayName: name?.current.value,
+            photoURL: "https://avatars.githubusercontent.com/u/31695246?v=4",
+          })
+            .then(() => {
+              // Profile updated!
+              console.log(user);
+              const { uid, email, displayName, photoURL } = auth.currentUser;
+              dispatch(
+                addUser({
+                  uid: uid,
+                  email: email,
+                  displayName: displayName,
+                  photoURL: photoURL,
+                })
+              );
+              navigate("/browse");
+            })
+            .catch((error) => {
+              // An error occurred
+              setErrorMessage(error.message);
+            });
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorCode + "-" + errorMessage);
+        });
+    } else {
+      //Sign in logic
+
+      signInWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          console.log(user);
+          navigate("/browse");
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorCode + "-" + errorMessage);
+        });
+    }
   };
   return (
     <div>
@@ -45,7 +123,8 @@ const Login = () => {
           {isSignInForm ? "Sign In" : "Sign Up"}
         </h1>
         {!isSignInForm && (
-          <input ref={name}
+          <input
+            ref={name}
             type="text"
             placeholder="Full Name"
             className="p-4 my-4 w-full bg-gray-700"
@@ -63,13 +142,14 @@ const Login = () => {
           placeholder="Password"
           className="p-4 my-4 w-full  bg-gray-700"
         />
-        {!isSignInForm && (
-          <input ref={confirmPassword}
+        {/* {!isSignInForm && (
+          <input
+            ref={confirmPassword}
             type="password"
             placeholder="Confirm Password"
             className="p-4 my-4 w-full  bg-gray-700"
           />
-        )}
+        )} */}
         <p className="text-red-500 font-bold text-lg p-2">{errorMessage}</p>
         <button
           className="p-4 my-6 w-full  bg-red-700 rounded-lg cursor-pointer"
